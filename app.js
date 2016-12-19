1dfccaa62383b877a89b9668f809a060c6878cec
leon <leon@themrolab-singapore.com>
2016-12-19 09:40:08 +0800

    update array generation

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ angular
     // synchronize the object with a three-way data binding
     // click on `index.html` above to see it used in the DOM!
     syncObject.$bindTo($scope, "data");
-
-    $scope.labels = [1, 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+    var arr = [];
+    for(var i=1; i<=20; i++){
+        arr.push(i);
+    }
+    $scope.labels = arr;
 });
